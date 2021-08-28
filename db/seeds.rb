2d58e7d0d686521fc8229da8a20effae3e0bb94b
Alieu Baldeh <alieu.dev@gmail.com>
2021-08-28 17:33:31 -0400

    completed lab

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create(first_name: 'Alieu', last_name: 'Baldeh', grade: 100)
+Student.create(first_name: 'Emily', last_name: 'Sylva', grade: 100)
